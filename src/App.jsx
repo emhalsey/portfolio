80fcb6de81f668e6e5a02d4dca02125e201fb2ea
Emma Halsey <160976732+emhalsey@users.noreply.github.com>
2025-03-25 19:53:50 -0400

    updates :)

--- src/App.jsx
+++ src/App.jsx
@@ -7,31 +7,29 @@ function App() {
 
   return (
     <>
+      <h1>Emma Halsey</h1>
+      <div className="card">
+        <p>
+          Data Engineeering, Project Management, and Operations
+        </p>
+      </div>
       <div>
         <a href="https://linkedin.com/in/emhalsey" target="_blank">
         {/* opens linkedin in a new page */}
-          <img src={LinkedIn} className="logo" alt="LinkedIn logo" />
+          <img src={LinkedIn} className="logo linkedin" alt="LinkedIn logo" />
         </a>
         <a href="https://github.com/emhalsey" target="_blank">
-          <img src={GitHub} className="logo react" alt="GitHub logo" />
+          <img src={GitHub} className="logo github" alt="GitHub logo" />
         </a>
-      </div>
-      <h1>Emma Halsey</h1>
-      <div className="card">
-        <button onClick={() => scrollToSection(aboutRef)}>About</button>
-        <br></br>
-        <br></br>
-        <button onClick={() => scrollToSection(projectsRef)}>Projects</button>
-        <br></br>
-        <br></br>
-        <button onClick={() => scrollToSection(contactRef)}>Contact</button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <div>
+        <button onClick={() => scrollToSection(aboutRef)}>About</button>
+        <button onClick={() => scrollToSection(projectsRef)}>Projects</button>
+        <button onClick={() => scrollToSection(contactRef)}>Contact</button>
+      </div>
       
     </>
   )
